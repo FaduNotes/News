b240ac7efa1215bb306c6d89eccf0b48e295f6ba
Aadil <aadilzehen7549@gmail.com>
2026-05-26 13:41:10 +0530

    Update service worker cache name and URLs

--- sw.js
+++ sw.js
@@ -1,22 +1,22 @@
-const CACHE_NAME = 'babu-chat-v1';
+const CACHE_NAME = 'babu-chat-v3';
 const urlsToCache = [
-  '/Babu-chat/',
-  '/Babu-chat/index.html',
-  '/Babu-chat/manifest.json',
-  '/Babu-chat/icon-192.png',
-  '/Babu-chat/icon-512.png'
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+     .then(cache => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => response || fetch(event.request))
+     .then(response => response || fetch(event.request))
   );
 });
