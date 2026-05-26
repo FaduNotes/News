17d7b84238c0e9d4817f12d1a285f9c124479a29
Aadil <aadilzehen7549@gmail.com>
2026-05-26 14:02:11 +0530

    Refactor service worker event listeners

--- sw.js
+++ sw.js
@@ -1,22 +1,3 @@
-const CACHE_NAME = 'babu-chat-v3';
-const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-     .then(cache => cache.addAll(urlsToCache))
-  );
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-     .then(response => response || fetch(event.request))
-  );
-});
+self.addEventListener('install', e => self.skipWaiting());
+self.addEventListener('activate', e => e.waitUntil(clients.claim()));
+self.addEventListener('fetch', e => {});
